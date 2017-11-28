23d8c8ba412260c10e6717df7d7853530e7b0fd3
Tim Jones <timjones10@hotmail.com>
2017-11-28 12:25:21 +0000

    Tests added for Once the sell by date has passed, Quality degrades twice as fast, The Quality of an item is never negative ,“Aged Brie” actually increases in Quality the older it gets. All three tests pass

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,4 +10,30 @@ describe GildedRose do
     end
   end
 
+  describe "#update_quality" do
+    it "does not allow a quality of below 0" do
+      items = [Item.new("Normal", 5, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+  end
+
+  describe "update_quality" do
+    it "quality degrades twice as fast once sell by date has passed" do
+      items = [Item.new("Normal", 0, 2)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+  end
+
+  describe "update_quality" do
+    it "Aged Brie increases in Quality the older it gets" do
+      items = [Item.new("Aged Brie", 1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 2
+    end
+  end
+
+
+
 end
